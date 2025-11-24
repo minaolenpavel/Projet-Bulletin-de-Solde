bb7a983a4658e4ffa40971a5e3c04f93605e71e4
Paul <bellevillepaul03@gmail.com>
2025-11-24 13:57:26 +0100

    started blazor app

---
+++ blazor/Bulletin_solde/Bulletin_solde/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Bulletin_solde.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulletin_solde.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Bulletin> Bulletin { get; set; }
+    }
+}
---
+++ blazor/Bulletin_solde/Bulletin_solde/Data/Models/Bulletin.cs
@@ -0,0 +1,14 @@
+﻿namespace Bulletin_solde.Data.Models
+{
+    public class Bulletin
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public string MonthText {  get; set; }
+        public string Period {  get; set; }
+        public decimal Amount { get; set; }
+        public string FilePath {  get; set; }
+
+    }
+}
