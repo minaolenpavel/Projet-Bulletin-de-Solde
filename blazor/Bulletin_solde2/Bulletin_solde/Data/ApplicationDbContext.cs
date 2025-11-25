ba5955f5b9dfb358a8ed092b507969dd6485268a
pavel <bellevillepaul03@gmail.com>
2025-11-25 14:51:42 +0100

    changed project, now can manually import json

---
+++ blazor/Bulletin_solde2/Bulletin_solde/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bulletin_solde.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Bulletin_solde.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Bulletin> Bulletins { get; set; }
+    }
+}
---
+++ blazor/Bulletin_solde2/Bulletin_solde/Data/Service/JsonServices.cs
@@ -0,0 +1,35 @@
+﻿using Bulletin_solde.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace Bulletin_solde.Data.Services
+{
+    public class JsonServices
+    {
+        private readonly ApplicationDbContext _context;
+        public JsonServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Bulletin>> GetAllBulletins()
+        {
+            return await _context.Bulletins.ToListAsync();
+        }
+
+        public async Task AddBulletin(Bulletin bulletin)
+        {
+            _context.Bulletins.Add(bulletin);
+            await _context.SaveChangesAsync();
+        }
+
+        public Bulletin? ParseJson(string jsonString)
+        {
+            Bulletin? bulletin = null;
+            bulletin = JsonConvert.DeserializeObject<Bulletin>(jsonString);
+            return bulletin;
+        }
+
+    }
+}
