7b7415cfcad5be236b2b120df81f3265ee73c30d
pavel <bellevillepaul03@gmail.com>
2025-12-02 14:08:35 +0100

    patched error when db is empty fixed stats not updating when we import manually to the empty db. Charts still don't update

--- blazor/Bulletin_solde2/Bulletin_solde/Data/Service/StatisticsServices.cs
+++ blazor/Bulletin_solde2/Bulletin_solde/Data/Service/StatisticsServices.cs
@@ -20,11 +20,20 @@ namespace Bulletin_solde.Data.Service
             double total = await _context.Bulletins.Select(b => b.Amount).SumAsync();
             return Math.Round(total,2);
         }
-
         public async Task<double> GetAverageIncome()
         {
             double average = await _context.Bulletins.Select(b => b.Amount).AverageAsync();
             return Math.Round(average, 2);
+        }
+        public async Task<double> GetMinStartIncome()
+        {
+            double minStart = await _context.Bulletins.Select(b => b.Amount).MinAsync();
+            return Math.Round(minStart, 2);
+        }
+        public async Task<double> GetMaxEndIncome()
+        {
+            double maxEnd = await _context.Bulletins.Select(b => b.Amount).MaxAsync();
+            return Math.Round(maxEnd, 2);
         }
     }
 }
